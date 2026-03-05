757717cfb36d440a26d4e10d32fda5d38513a8c1
Asim-AKM <asimkhanmiani9@gmail.com>
2026-03-05 05:06:52 +0500

    Add StudentEmail and StudentName to Students table
    Introduced UpdateStudentEntity migration to add two required columns, StudentEmail and StudentName, to the Students table. Updated model snapshot and migration logic to support adding and removing these fields. Columns are non-nullable and use nvarchar(max) type.

---
+++ Infrastructure_Service/Persistance/Migrations/20260305000127_UpdateStudentEntity.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure_Service.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateStudentEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StudentEmail",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StudentName",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StudentEmail",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "StudentName",
+                table: "Students");
+        }
+    }
+}
